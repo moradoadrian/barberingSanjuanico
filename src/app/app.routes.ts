973fef30f570df05c2bc8b61a3dc0dcf7c42700e
Adrián Morado <moradoadrian@gmail.com>
2025-12-17 14:13:17 -0600

    ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,6 @@ import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'home'
-    },
-    {
         path: 'home',
         component: HomeComponent
     },
